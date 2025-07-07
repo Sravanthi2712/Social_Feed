0becff0f539e87ebc177dc8dad45c0a9579e1162
Sravanthi2712 <137677948+Sravanthi2712@users.noreply.github.com>
2025-07-07 10:02:38 +0530

    Update Login.jsx

--- HRMS/client/src/components/Login.jsx
+++ HRMS/client/src/components/Login.jsx
@@ -31,7 +31,7 @@ const Login = ({setAuthMode,setUser}) => {
       <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
         <div className="max-w-md w-full bg-white rounded-xl shadow-lg p-8">
           <h2 className="text-2xl font-bold text-gray-900 mb-6 text-center">
-            Sign In
+            Welcome👋
           </h2>
 
           <form className="space-y-4" onSubmit={handleSubmit}>
@@ -75,12 +75,12 @@ const Login = ({setAuthMode,setUser}) => {
               className="w-full bg-indigo-600 hover:bg-indigo-700 text-white font-medium py-2.5 rounded-lg transition-colors"
               disabled={Loding}
             >
-              {Loding ? "Loading..." : "Sign Up"}
+              {Loding ? "Loading..." : "Sign In"}
             </button>
           </form>
 
           <div className="mt-6 text-center text-sm text-gray-600">
-            Already have an account?
+            Donot have an account?
             <div
             onClick={()=>{setAuthMode("regester")}}
               className="text-indigo-600 hover:text-indigo-500 font-medium"
